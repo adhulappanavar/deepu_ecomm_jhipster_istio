db0371d239fd97870c134c8d16d46393eff3482f
Anil Dhulappanavar <anil.dhulappanavar.2015@gmail.com>
2019-10-10 11:20:15 +0530

    changed the directory

--- build_n_containerize.sh
+++ build_n_containerize.sh
@@ -1,9 +1,9 @@
 
-cd /Users/anidhula/learn/recording/deepu_hello_hipster/store
+cd /Users/anidhula/learn/recording/deepu_ecomm_jhipster_istio/store
 ./gradlew bootJar -Pprod jibDockerBuild
-cd /Users/anidhula/learn/recording/deepu_hello_hipster/invoice
+cd /Users/anidhula/learn/recording/deepu_ecomm_jhipster_istio/invoice
 ./gradlew bootJar -Pprod jibDockerBuild
-cd /Users/anidhula/learn/recording/deepu_hello_hipster/notification
+cd /Users/anidhula/learn/recording/deepu_ecomm_jhipster_istio/notification
 ./gradlew bootJar -Pprod jibDockerBuild
-cd /Users/anidhula/learn/recording/deepu_hello_hipster/product
+cd /Users/anidhula/learn/recording/deepu_ecomm_jhipster_istio/product
 ./gradlew bootJar -Pprod jibDockerBuild
