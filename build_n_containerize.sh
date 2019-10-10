8068e7491491e7b9cc973f5da13c370d264f3eea
Anil Dhulappanavar <anil.dhulappanavar.2015@gmail.com>
2019-10-10 11:15:40 +0530

    Initial Version

---
+++ build_n_containerize.sh
@@ -0,0 +1,9 @@
+
+cd /Users/anidhula/learn/recording/deepu_hello_hipster/store
+./gradlew bootJar -Pprod jibDockerBuild
+cd /Users/anidhula/learn/recording/deepu_hello_hipster/invoice
+./gradlew bootJar -Pprod jibDockerBuild
+cd /Users/anidhula/learn/recording/deepu_hello_hipster/notification
+./gradlew bootJar -Pprod jibDockerBuild
+cd /Users/anidhula/learn/recording/deepu_hello_hipster/product
+./gradlew bootJar -Pprod jibDockerBuild
---
+++ publishcontainers_to_dockerhub.sh
@@ -0,0 +1,9 @@
+
+docker image tag store adhulappanavar/store
+docker push adhulappanavar/store
+docker image tag invoice adhulappanavar/invoice
+docker push adhulappanavar/invoice
+docker image tag notification adhulappanavar/notification
+docker push adhulappanavar/notification
+docker image tag product adhulappanavar/product
+docker push adhulappanavar/product
